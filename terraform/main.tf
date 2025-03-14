c4304b89c253fabd71e1ebeddd23af67e9a1a8a5
ZitouneMcGregor <carreregee.olivier@gmail.com>
2025-03-14 15:14:01 +0100

    bdd + app qui marchent alors que clément pas du tout

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,20 @@
-# module "app" {
-#   source = "./app"
+module "app" {
+  source = "./app"
 
-#   vm_name       = "cytech-2-app"
-#   dedicated_mem = 512
-#   vcpu_number   = 1
-#   ip_vm = "192.168.100.122/24"
-#   ssh_key = "/home/cytech/.ssh/id_rsa.pub"
+  vm_name       = "cytech-2-app"
+  dedicated_mem = 512
+  vcpu_number   = 1
+  ip_vm         = "192.168.100.122/24"
+  ssh_key       = "/home/cytech/.ssh/id_rsa.pub"
 
 
-#   node_name    = "mgmt"
-#   pool_id      = "cytech"
-#   bridge       = "vmbr0"
-#   stop_destroy = true
-#   count_vm     = 1
+  node_name    = "mgmt"
+  pool_id      = "cytech"
+  bridge       = "vmbr0"
+  stop_destroy = true
+  count_vm     = 1
 
-# }
+}
 
 module "bdd" {
   source = "./bdd"
@@ -22,8 +22,8 @@ module "bdd" {
   vm_name       = "cytech-2-bdd"
   dedicated_mem = 2048
   vcpu_number   = 1
-  ssh_key = "/home/cytech/.ssh/id_rsa.pub"
-  ip_vm = "192.168.100.123/24"
+  ssh_key       = "/home/cytech/.ssh/id_rsa.pub"
+  ip_vm         = "192.168.100.123/24"
 
 
 
