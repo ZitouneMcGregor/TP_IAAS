503734d08bb2f4f37c541fe39910b3043ae5c83a
ZitouneMcGregor <carreregee.olivier@gmail.com>
2025-03-16 14:09:17 +0100

    format

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,11 +6,11 @@ module "app" {
   vcpu_number   = var.app_vcpu_number
   ip_vm         = var.app_ip_vm
   ssh_key       = var.app_ssh_key
-  node_name    = var.app_node_name
-  pool_id      = var.app_pool_id
-  bridge       = var.app_bridge
-  stop_destroy = var.app_stop_destroy
-  count_vm     = var.app_count_vm
+  node_name     = var.app_node_name
+  pool_id       = var.app_pool_id
+  bridge        = var.app_bridge
+  stop_destroy  = var.app_stop_destroy
+  count_vm      = var.app_count_vm
 
 }
 
@@ -22,18 +22,18 @@ module "bdd" {
   vcpu_number   = var.bdd_vcpu_number
   ip_vm         = var.bdd_ip_vm
   ssh_key       = var.bdd_ssh_key
-  node_name    = var.bdd_node_name
-  pool_id      = var.bdd_pool_id
-  bridge       = var.bdd_bridge
-  stop_destroy = var.bdd_stop_destroy
-  count_vm     = var.bdd_count_vm
+  node_name     = var.bdd_node_name
+  pool_id       = var.bdd_pool_id
+  bridge        = var.bdd_bridge
+  stop_destroy  = var.bdd_stop_destroy
+  count_vm      = var.bdd_count_vm
 
 }
 
 resource "local_file" "ansible_inventory" {
 
   filename = "${path.module}/../ansible/inventory.ini"
-  content = <<EOF
+  content  = <<EOF
 [app]
 host-app ansible_host=${chomp(var.app_ip_vm_inv)} ansible_user=ubuntu ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 
@@ -44,7 +44,7 @@ EOF
 
 resource "null_resource" "run_ansible" {
   depends_on = [
-    module.app, 
+    module.app,
     module.bdd,
     local_file.ansible_inventory
   ]
